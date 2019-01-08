364c96d680c6af969da828858e44d4cc2e92d1d8
Ipul Saepulloh <ipulsaepulloh9@gmail.com>
2019-01-08 12:15:47 +0700

    update function

--- src/App.js
+++ src/App.js
@@ -4,15 +4,38 @@ import './App.css';
 
 
 class App extends Component {
+  state = {
+    todos: [],
+    todo: ""
+  }
+
+  onChange = e => {
+    this.setState({
+      todo: e.target.value
+    })
+  }
+
+  onSubmit = e => {
+    this.setState(prevState => ({
+      todos: prevState.todos.concat(this.state.todo)
+    }))
+  }
+
   render() {
+
     return (
       <div className="App">
         <img src={logo} alt="Logo React" className="App-logo" />
         <div id="todo" class="header">
           <h1>Todo List </h1>
-          <input type="text" id="input" placeholder="input  activity" />
-          <span onclick="newElement()" class="addBtn">Add</span>
+          <input onChange={this.onChange} value={this.state.todo} type="text" id="input" placeholder="input  activity" />
+          <span onClick={this.onSubmit} class="addBtn">Add</span>
         </div>
+        {
+          this.state.todos.map((todo, i) => (
+            <p key={i}>{todo}</p>
+          ))
+        }
       </div>
     );
   }
