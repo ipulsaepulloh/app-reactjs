b7487cf6242f1e7fc80ab836bb8d6dee4f6fc879
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-10 16:45:06 +0700

    Fix styling in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 class App extends Component {
   state = {
@@ -19,19 +18,17 @@ class App extends Component {
     this.setState(prevState => ({
       todos: prevState.todos.concat({ text: this.state.todo, done: false }),
       todo: ""
-
     }))
   }
 
   handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
+    const target = event.target
+    const value = target.type === "checkbox" ? target.checked : target.value
+    const name = target.name
 
     this.setState({
       [name]: value
-    });
-
+    })
   }
 
   finishTodo = i => {
@@ -55,32 +52,48 @@ class App extends Component {
   }
 
   render() {
-
     return (
       <div className="App">
         <div id="todo" className="header">
           <h1>Todo List App</h1>
-          <ul>
-            {
-              this.state.todos.map((todo, i) => (
-                <li key={i}><form onClick={() => this.finishTodo(i)} style={{ textDecoration: todo.done && 'line-through' }}>{todo.text}</form>
-                  <input
-                    name="isGoing"
-                    type="checkbox"
-                    checked={this.state.isGoing}
-                    onChange={this.handleInputChange} />
-                  <span onClick={() => this.deleteTodo(i)} className="delBtn">x</span>
-                </li>
-              ))
-            }
-          </ul>
-          <input onChange={this.onChange} value={this.state.todo} type="text" id="input" placeholder="Input  Activity" />
-          <form onClick={this.onSubmit} className="addBtn">+</form>
-        </div>
 
+          <form onSubmit={this.onSubmit}>
+            <input
+              onChange={this.onChange}
+              value={this.state.todo}
+              type="text"
+              id="input"
+              placeholder="Input  Activity"
+              className="todo-input"
+            />
+            <input type="submit" className="addBtn" value="+" />
+          </form>
+
+          <ul>
+            {this.state.todos.map((todo, i) => (
+              <li key={i}>
+                <span
+                  onClick={() => this.finishTodo(i)}
+                  style={{ textDecoration: todo.done && "line-through" }}
+                >
+                  {todo.text}
+                </span>
+                <input
+                  name="isGoing"
+                  type="checkbox"
+                  checked={this.state.isGoing}
+                  onChange={this.handleInputChange}
+                />
+                <span onClick={() => this.deleteTodo(i)} className="delBtn">
+                  x
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
