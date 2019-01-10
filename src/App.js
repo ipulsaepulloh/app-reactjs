d04ef5c58325fb0279b9dedeb5ba93919de24067
Ipul Saepulloh <ipulsaepulloh9@gmail.com>
2019-01-10 14:50:04 +0700

    update interface apps

--- src/App.js
+++ src/App.js
@@ -23,6 +23,17 @@ class App extends Component {
     }))
   }
 
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+
+  }
+
   finishTodo = i => {
     const updatedTodos = this.state.todos.slice()
 
@@ -47,22 +58,26 @@ class App extends Component {
 
     return (
       <div className="App">
-        <img src={logo} alt="Logo React" className="App-logo" />
         <div id="todo" className="header">
-          <h1>Todo List </h1>
+          <h1>Todo List App</h1>
+          <ul>
+            {
+              this.state.todos.map((todo, i) => (
+                <li key={i}><form onClick={() => this.finishTodo(i)} style={{ textDecoration: todo.done && 'line-through' }}>{todo.text}</form>
+                  <input
+                    name="isGoing"
+                    type="checkbox"
+                    checked={this.state.isGoing}
+                    onChange={this.handleInputChange} />
+                  <span onClick={() => this.deleteTodo(i)} className="delBtn">x</span>
+                </li>
+              ))
+            }
+          </ul>
           <input onChange={this.onChange} value={this.state.todo} type="text" id="input" placeholder="Input  Activity" />
-          <form onClick={this.onSubmit} className="addBtn">Add</form>
+          <form onClick={this.onSubmit} className="addBtn">+</form>
         </div>
 
-        <ul>
-          {
-            this.state.todos.map((todo, i) => (
-              <li key={i}><form onClick={() => this.finishTodo(i)} style={{ textDecoration: todo.done && 'line-through' }}>{todo.text}</form>
-                <span onClick={() => this.deleteTodo(i)} className="delBtn">x</span>
-              </li>
-            ))
-          }
-        </ul>
       </div>
     );
   }
